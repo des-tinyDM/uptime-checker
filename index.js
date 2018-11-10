b8f183d167657885908a12ece916fd61d37bb7b9
des-tinyDM <destinyleaross@gmail.com>
2018-11-10 10:03:16 -0600

    added functionality to parse path, query strings, methods, and headers from a request

--- index.js
+++ index.js
@@ -3,10 +3,38 @@
 
 // Dependencies
 const http = require("http");
+const url = require("url");
 
 // Server initialization on port 3010
 const server = http.createServer((req, res) => {
+  // Parse url request
+  const parsedUrl = url.parse(req.url, true);
+
+  //Get the path from url
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Get query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  const headers = req.headers;
+  //Get the http method
+  const method = req.method.toLowerCase();
+
+  //Send response
   res.end("Hello World\n");
+
+  //Log path
+  console.log(
+    "request received on path:",
+    trimmedPath,
+    "with method:",
+    method,
+    "and these query string parameters:",
+    queryStringObject,
+    ". Request received with these headers:",
+    headers
+  );
 });
 
 server.listen(3000, () => {
